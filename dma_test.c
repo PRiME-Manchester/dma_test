ed2a11740f2dba52129d04f74d9832329a0c768f
Patrick Camilleri <patrick.camilleri@manchester.ac.uk>
2015-10-02 13:46:15 +0200

    working dma transfers at high speed

--- dma_test.c
+++ dma_test.c
@@ -31,10 +31,11 @@
 // ------------------------------------------------------------------------
 
 #define TIMER_TICK_PERIOD  1000000
-#define TOTAL_TICKS        10
-#define ITER_TIMES         16
+#define TOTAL_TICKS        4
+#define ITER_TIMES         1
 
-#define BUFFER_SIZE        1000
+#define BUFFER_SIZE        4000
+#define DMA_REPS           10000
 
 // ------------------------------------------------------------------------
 // variablesqueu
@@ -108,13 +109,13 @@ void app_init ()
   // and a buffer somewhere in SDRAM
 
   sdram_buffer = (uint *) sark_xalloc (sv->sdram_heap,
-					BUFFER_SIZE * sizeof(uint),
+					(BUFFER_SIZE+1) * sizeof(uint),
 					0,
 					ALLOC_LOCK);
 
   // and a buffer in DTCM
 
-  dtcm_buffer = (uint *) sark_alloc (BUFFER_SIZE, sizeof(uint));
+  dtcm_buffer = (uint *) sark_alloc (BUFFER_SIZE+1, sizeof(uint));
 
   if (dtcm_buffer == NULL ||  sdram_buffer == NULL)
   {
@@ -125,15 +126,15 @@ void app_init ()
   {
     test_DMA = TRUE;
     // initialize sections of DTCM, system RAM and SDRAM
-    for (uint i = 0; i < BUFFER_SIZE; i++)
-      {
-	dtcm_buffer[i]   = i;
-	//sysram_buffer[i] = 0xa5a5a5a5;
-	sdram_buffer[i]  = 0x5a5a5a5a;
+    for (uint i = 0; i < BUFFER_SIZE+1; i++)
+    {
+      dtcm_buffer[i]   = BUFFER_SIZE-i;
+      //sysram_buffer[i] = 0xa5a5a5a5;
+      //sdram_buffer[i]  = 0x5a5a5a5a;
+      sdram_buffer[i]  = 0;
     }
 
-    io_printf (IO_BUF, "[core %d] dtcm buffer @ 0x%08x\n", coreID,
-               (uint) dtcm_buffer);
+    io_printf (IO_BUF, "[core %d] dtcm buffer @ 0x%08x sdram buffer @ 0x%08x\n", coreID, (uint) dtcm_buffer, (uint)sdram_buffer);
   }
 }
 /*
@@ -171,15 +172,6 @@ void app_done ()
 
   // report number of failed transfers
   io_printf (IO_BUF, "[core %d] failed %d DMA transfers\n", coreID, tfailed);
-
-  // report number of scheduled DMA transfers
-  io_printf (IO_BUF, "[core %d] scheduled %d DMA transfers\n", coreID, count);
-
-  // report number of scheduled DMA transfers
-  //io_printf (IO_BUF, "[core %d] scheduled %d DMA transfers (val=3)\n", coreID, count2);
-
-  // report number of scheduled DMA transfers
-  //io_printf (IO_BUF, "[core %d] failed %d DMA transfers (val=3)\n", coreID, count_failed);
 
   if (tfailed)
   {
@@ -261,6 +253,76 @@ void flip_led (uint ticks, uint null)
 /*
 *******/
 
+// reverses a string 's' of length 'len'
+void reverse(char *s, int len)
+{
+    int i=0, j=len-1;
+    char temp;
+    while (i<j)
+    {
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        i++;
+        j--;
+    }
+}
+
+// Converts a given integer num to string str[].  len is the number
+ // of digits required in output. If len is more than the number
+ // of digits in num, then 0s are added at the beginning.
+uint itoa(uint num, char s[], uint len)
+{
+    uint i = 0;
+
+    do {
+        s[i++] = '0' + num%10;
+        num /= 10;
+    } while (num>0);
+
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < len)
+      s[i++] = '0';
+ 
+    reverse(s, i);
+    s[i] = '\0';
+    return i;
+}
+
+
+
+// Converts a floating point number to string.
+void ftoa(float n, char *res, int precision)
+{
+  // Extract integer part
+  int ipart = (int)n;
+
+  // Extract floating part
+  float fpart = n - (int)n;
+
+  // convert integer part to string
+  uint i = itoa(ipart, res, 0);
+
+  // check for display option after point
+  if (precision != 0)
+  {
+    res[i] = '.';  // add dot
+
+    // This computes pow(10,precision)
+    uint m=1;
+    if (precision>0)
+    for (int i=0; i<precision; i++)
+      m*=10;
+
+    // Get the value of fraction part upto given no.
+    // of points after dot. The third parameter is needed
+    // to handle cases like 233.007
+    fpart = fpart * m;
+
+    itoa((int)fpart, res+i+1, precision);
+  }
+}
 
 /****f* simple.c/do_transfer
 *
@@ -280,19 +342,54 @@ void flip_led (uint ticks, uint null)
 void do_transfer (uint val, uint none)
 {
   uint transfer_id;
+  uint t1, t2;
+  char tput_s[20], mb_s[20];
 
-  //io_printf(IO_BUF, "Transfer %d\n", count++);
-  if (val == 3)
+  if (val == 1)
   {
-    count2++;
-    //transfer_id = spin1_dma_transfer(DMA_WRITE, sysram_buffer, dtcm_buffer, DMA_WRITE,
-    //                   BUFFER_SIZE*sizeof(uint));
-    //io_printf(IO_BUF, "tid: %d A\n", transfer_id);
+    io_printf(IO_BUF, "\n1. Performing DMA Write (DTCM->SDRAM)\n");
+    //transfer_id = spin1_dma_transfer_crc(DMA_WRITE, sdram_buffer, dtcm_buffer, CRC_WRITE, DMA_WRITE, BUFFER_SIZE*sizeof(uint));
+    
+    transfer_id = spin1_dma_transfer(DMA_WRITE, sdram_buffer, dtcm_buffer, DMA_WRITE, BUFFER_SIZE*sizeof(uint));
+
+    // Wait for DMA operation to finish
+    while((dma[DMA_STAT]&0x01));
+
+/*
+    io_printf(IO_BUF, "CRCC:%08x CRCR:%08x CRC error:%d SDRAM: %08x %08x %08x %08x\n\n", dma[DMA_CRCC], dma[DMA_CRCR], dma[DMA_STAT & 0x0d] >> 13, sdram_buffer[0], sdram_buffer[1], sdram_buffer[99], sdram_buffer[BUFFER_SIZE]);
+*/
+
+    io_printf(IO_BUF, "2. Corrupting SDRAM\n");
+    
+    sdram_buffer[402] ^= sdram_buffer[402]; //0x0f0f0f0f;
+    //sdram_buffer[400] ^= 0xf0f0f0f0;
   }
-  else if (val == 5)
+  else if (val == 2)
   {
-    transfer_id = spin1_dma_transfer(DMA_WRITE, sdram_buffer, dtcm_buffer, DMA_WRITE,
-		       BUFFER_SIZE*sizeof(uint));
+    io_printf(IO_BUF, "3. Performing DMA Read (SDRAM->DTCM)\n");
+    
+    t1 = sv->clock_ms;
+    for(uint i=0; i<DMA_REPS; i++)
+    {
+      //transfer_id = spin1_dma_transfer_crc(DMA_READ, sdram_buffer, dtcm_buffer, CRC_WRITE, DMA_READ, BUFFER_SIZE*sizeof(uint));
+
+      transfer_id = spin1_dma_transfer(DMA_READ, sdram_buffer, dtcm_buffer, DMA_READ, BUFFER_SIZE*sizeof(uint));
+
+      // Wait for DMA operation to finish
+      while((dma[DMA_STAT]&0x01));
+    }
+
+    t2 = sv->clock_ms;
+    io_printf(IO_BUF, "CRCC:%08x CRCR:%08x CRC error:%d\n\n", dma[DMA_CRCC], dma[DMA_CRCR], (dma[DMA_STAT] >> 13)&0x01);
+    //io_printf(IO_BUF, "t2-t1:%d ms\n", t2-t1);
+    
+    ftoa(BUFFER_SIZE*4.0*DMA_REPS/(t2-t1)/1000.0, tput_s, 2);
+    ftoa(BUFFER_SIZE*4.0*DMA_REPS/1000000.0, mb_s, 2);
+    io_printf(IO_BUF, "Throughput: %s MB/s (%s MB in %d ms) Transfers:%d\n\n", tput_s, mb_s, t2-t1, transfers);
+/*
+    io_printf(IO_BUF, "CRCC:%08x CRCR:%08x CRC error:%d  DTCM: %08x %08x %08x %08x\n\n", dma[DMA_CRCC], dma[DMA_CRCR], dma[DMA_STAT & 0x0d] >> 13, dtcm_buffer[0], dtcm_buffer[1], dtcm_buffer[99], dtcm_buffer[BUFFER_SIZE]);
+*/
+
     //io_printf(IO_BUF, "tid: %d B\n", transfer_id);
   }
 }
@@ -321,6 +418,8 @@ void count_packets (uint key, uint payload)
 
   // count packets
   packets++;
+
+  //io_printf(IO_BUF, "Payload: %d\n", payload);
 
   // if testing DMA trigger transfer requests at the right time
   if (test_DMA)
@@ -410,6 +509,42 @@ void check_memcopy(uint tid, uint ttag)
 /*
 *******/
 
+// Configure CRC table
+void configure_crc_tables(void)
+{
+  dma[DMA_CRCT +   0] = 0xFB808B20;
+  dma[DMA_CRCT +   4] = 0x7DC04590;
+  dma[DMA_CRCT +   8] = 0xBEE022C8;
+  dma[DMA_CRCT +  12] = 0x5F701164;
+  dma[DMA_CRCT +  16] = 0x2FB808B2;
+  dma[DMA_CRCT +  20] = 0x97DC0459;
+  dma[DMA_CRCT +  24] = 0xB06E890C;
+  dma[DMA_CRCT +  28] = 0x58374486;
+  dma[DMA_CRCT +  32] = 0xAC1BA243;
+  dma[DMA_CRCT +  36] = 0xAD8D5A01;
+  dma[DMA_CRCT +  40] = 0xAD462620;
+  dma[DMA_CRCT +  44] = 0x56A31310;
+  dma[DMA_CRCT +  48] = 0x2B518988;
+  dma[DMA_CRCT +  52] = 0x95A8C4C4;
+  dma[DMA_CRCT +  56] = 0xCAD46262;
+  dma[DMA_CRCT +  60] = 0x656A3131;
+  dma[DMA_CRCT +  64] = 0x493593B8;
+  dma[DMA_CRCT +  68] = 0x249AC9DC;
+  dma[DMA_CRCT +  72] = 0x924D64EE;
+  dma[DMA_CRCT +  76] = 0xC926B277;
+  dma[DMA_CRCT +  80] = 0x9F13D21B;
+  dma[DMA_CRCT +  84] = 0xB409622D;
+  dma[DMA_CRCT +  86] = 0x21843A36;
+  dma[DMA_CRCT +  90] = 0x90C21D1B;
+  dma[DMA_CRCT +  94] = 0x33E185AD;
+  dma[DMA_CRCT +  98] = 0x627049F6;
+  dma[DMA_CRCT + 102] = 0x313824FB;
+  dma[DMA_CRCT + 106] = 0xE31C995D;
+  dma[DMA_CRCT + 110] = 0x8A0EC78E;
+  dma[DMA_CRCT + 114] = 0xC50763C7;
+  dma[DMA_CRCT + 118] = 0x19033AC3;
+  dma[DMA_CRCT + 122] = 0xF7011641;
+}
 
 /****f* simple.c/c_main
 *
@@ -429,7 +564,8 @@ void c_main()
   coreID = spin1_get_core_id ();
   chipID = spin1_get_chip_id ();
 
-  // Say hello...
+  // Setup CRC tables
+  configure_crc_tables();
 
   io_printf (IO_BUF, ">> simple - chip (%d, %d) core %d\n",
 	     chipID >> 8, chipID & 255, coreID);
@@ -441,7 +577,7 @@ void c_main()
   // register callbacks
 
   spin1_callback_on (MCPL_PACKET_RECEIVED, count_packets, -1);
-  spin1_callback_on (DMA_TRANSFER_DONE, check_memcopy, 0);
+  //spin1_callback_on (DMA_TRANSFER_DONE, check_memcopy, 0);
   spin1_callback_on (USER_EVENT, send_packets, 2);
   spin1_callback_on (TIMER_TICK, flip_led, 3);
 
